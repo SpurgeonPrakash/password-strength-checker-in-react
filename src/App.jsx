0c1bb695cd66e908be11959a4ae743c15ac386ac
Spurgeon Gnan Prakasham Tara <prakashamtara07@gmail.com>
2025-10-03 12:42:27 +0530

    Starter-Code

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import SignUp from "./components/SignUp/SignUp";
+
+function App() {
+  return <SignUp />;
+}
+
+export default App;
---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,36 @@
+import styles from "./SignUp.module.css";
+
+const SignUp = () => {
+  return (
+    <div className={`${styles.card}`}>
+      <h1 className={`${styles.header}`}>Sign Up</h1>
+      <form className={styles.form}>
+        <div className={styles.formGroup}>
+          <label htmlFor="username" className={styles.label}>
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            className={`${styles.input}`}
+            placeholder="Enter your username"
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="password" className={`${styles.label} `}>
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className={`${styles.input} `}
+            placeholder="Enter your password"
+          />
+        </div>
+        <button className={`${styles.submit}`}>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
